97645456fbd017d1a8d11ccc177a35885d5cdf39
Haikal Alfandi <kungkingkangcanyouseemekuking@gmail.com>
2023-10-23 05:15:03 -0400

    finished

--- index.js
+++ index.js
@@ -26,9 +26,31 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', function(req, res) {
+  let date;
+  if (!req.params.date) {
+    // If no date is provided, use the current date
+    date = new Date();
+  } else if (req.params.date.match(/^\d+$/)) {
+    // If the request is a unix timestamp, convert it to a number
+    let unixDate = parseInt(req.params.date);
+    date = new Date(unixDate);
+  } else {
+    // Otherwise, try to parse the date string
+    date = new Date(req.params.date);
+  }
 
+  if (isNaN(date.getTime())) {
+    // If the date is not valid, return an error
+    res.json({ error: "Invalid Date" });
+  } else {
+    // Otherwise, return the unix timestamp and UTC string
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
